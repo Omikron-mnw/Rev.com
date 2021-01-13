bdd3c219a17335b4dab365b17beb93c057788b8e
mnwhryk <minomushi.ff15.07.08@gmail.com>
2021-01-13 21:35:09 +0000

    comment/completed

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,19 +1,22 @@
 class Public::CommentsController < ApplicationController
 
-  def new
-  end
-
   def create
-  end
-
-  def index
-  end
-
-  def show
+    @review = Review.find(params[:review_id])
+    comment = current_user.comments.new(comment_params)
+    comment.review_id = @review.id
+    comment.save
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @review = Review.find(params[:review_id])
+    Comment.find_by(id: params[:id], review_id: params[:review_id]).destroy
+    redirect_back(fallback_location: root_path)
   end
 
+  private
+  def comment_params
+    params.require(:comment).permit(:review_id, :uer_id, :comment)
+  end
 
 end
