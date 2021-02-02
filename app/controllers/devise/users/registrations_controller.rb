5bf4444f32fb4dbd18e407389f2a378b7c4bf771
mnwhryk <minomushi.ff15.07.08@gmail.com>
2021-02-02 06:18:48 +0000

    sessoin-error-1

--- app/controllers/devise/users/registrations_controller.rb
+++ app/controllers/devise/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Devise::Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  before_action :forbid_test_user, only: %i[edit update destroy]
+  before_action :forbid_test_user, only: %i[:edit, :update, :destroy]
 
   # GET /resource/sign_up
   # def new
